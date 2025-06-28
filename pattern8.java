ab17363e34de162c0667470bbe81df5d327622f7
Sarthak-Bhoknal <sarthakbhoknal05@gmail.com>
2025-06-27 20:13:30 -0700

    Create pattern8.java

---
+++ pattern8.java
@@ -0,0 +1,21 @@
+public class pattern8 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j < 5; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k >= 1; k--) {
+                System.out.print(k);
+            }
+            for (int l = 2; l <= i; l++) {
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+}
+//     1
+//    212
+//   32123
+//  4321234
+// 543212345
